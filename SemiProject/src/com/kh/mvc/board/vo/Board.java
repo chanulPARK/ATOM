0152fccebbeee4cd8ee4655067d2d3a6e2a93a83
최형석 <saekeumnewsong@github.com>
2018-07-20 20:59:16 +0900

    최근 파일

---
+++ SemiProject/src/com/kh/mvc/board/vo/Board.java
@@ -0,0 +1,83 @@
+package com.kh.mvc.board.vo;
+import java.sql.*;
+
+public class Board {
+	private int boardNo;		// 게시글 번호
+	private String title;		// 글 제목
+	private Date writeDate;		// 작성일
+	private String content;		// 내용
+	private String writer;		// 작성자
+	private int visits;			// 조회수
+	
+	public Board() {
+		
+	}
+	
+	public Board(int boardNo, String title, Date writeDate,
+			String content, String writer, int visits) {
+		this.boardNo = boardNo;
+		this.title = title;
+		this.writeDate = writeDate;
+		this.content = content;
+		this.writer = writer;
+		this.visits = visits;
+	}
+
+	public int getBoardNo() {
+		return boardNo;
+	}
+
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getWriteDate() {
+		return writeDate;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public void setWriteDate(Date writeDate) {
+		this.writeDate = writeDate;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public int getVisits() {
+		return visits;
+	}
+
+	public void setVisits(int visits) {
+		this.visits = visits;
+	}
+
+	@Override
+	public String toString() {
+		return "번호:" + boardNo
+			+ ", 제목:" + title
+			+ ", 작성일:" + writeDate
+			+ ", 내용: " + content
+			+ ", 작성자:" + writer
+			+ ", 조회수:" + visits;
+	}
+}
---
+++ SemiProject/src/com/kh/mvc/controller/BoardUpdateServlet.java
@@ -0,0 +1,40 @@
+package com.kh.mvc.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BoardUpdateServlet
+ */
+@WebServlet(name="BoardUpdateServlet", urlPatterns="/boardUpdate")
+public class BoardUpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardUpdateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("BoardUpdateServlet 클래스에서 실행하였습니다.");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
